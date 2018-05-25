69a6e060199e4e0bb5b25422de077089f760e374
Ishani <ishaniamin@gmail.com>
2018-05-25 01:26:22 +0000

    Done.

--- trip.rb
+++ trip.rb
@@ -1,1 +1,14 @@
 # Code your prompts here!
+
+puts "Welcome!"
+
+puts "Where would you like to stay?"
+stay = gets.chomp.capitalize
+
+puts "What food do you want to eat?"
+eat = gets.chomp
+
+puts "How many nights would you like to stay?"
+nights = gets.chomp
+
+puts "#{stay} #{eat} #{nights}"
